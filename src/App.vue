4b73c28c760987cc28617003be086b9b0eec7924
lizeming <12785223@qq.com>
2021-04-29 08:58:37 +0800

    feat(components): globalHeader 头部导航栏 静态实现登录状态切换

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,44 @@
 <template>
   <div class="container">
+    <GlobalHeader :user="userInfo" @userLogin="userLoign" />
     <ColumnList :list="list" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
 
 const testData: ColumnProps[] = [
   {
     id: 1,
     title: 'test1的专栏',
     description: '这是的test1专栏，有一段非常有意思的简介，可以更细腻一下哦',
-    avantar: 'https://img.mukewang.com/5cf763de0001e42804000400-100-100.jpg?s=290'
+    avantar:
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20181223%2F19f8b83848b44920808942ba4442709d.jpeg&refer=http%3A%2F%2F5b0988e595225.cdn.sohucs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1622247138&t=9be7f6ba8093d1cd53547979bf640f51'
   },
   {
     id: 2,
     title: 'test2的专栏',
     description: '这是的test2专栏，有一段非常有意思的简介，可以更细腻一下哦',
-    avantar: 'https://img.mukewang.com/5cf763de0001e42804000400-100-100.jpg?s=290'
+    avantar:
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20181223%2F19f8b83848b44920808942ba4442709d.jpeg&refer=http%3A%2F%2F5b0988e595225.cdn.sohucs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1622247138&t=9be7f6ba8093d1cd53547979bf640f51'
   },
   {
     id: 3,
     title: 'test3的专栏',
     description: '这是的test3专栏，有一段非常有意思的简介，可以更细腻一下哦',
-    avantar: 'https://img.mukewang.com/5cf763de0001e42804000400-100-100.jpg?s=290'
+    avantar:
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20181223%2F19f8b83848b44920808942ba4442709d.jpeg&refer=http%3A%2F%2F5b0988e595225.cdn.sohucs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1622247138&t=9be7f6ba8093d1cd53547979bf640f51'
   },
   {
     id: 4,
     title: 'test4的专栏',
     description: '这是的test4专栏，有一段非常有意思的简介，可以更细腻一下哦',
-    avantar: 'https://img.mukewang.com/5cf763de0001e42804000400-100-100.jpg?s=290'
+    avantar:
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20181223%2F19f8b83848b44920808942ba4442709d.jpeg&refer=http%3A%2F%2F5b0988e595225.cdn.sohucs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1622247138&t=9be7f6ba8093d1cd53547979bf640f51'
   },
   {
     id: 5,
@@ -45,12 +51,26 @@ export default defineComponent({
   name: 'App',
   setup() {
     const list = ref<ColumnProps[]>(testData)
+
+    const userInfo: UserProps = reactive({
+      isLogin: false,
+      name: 'Viking',
+      id: 12785223
+    })
+    const userLoign = () => {
+      userInfo.isLogin = true
+      console.log(userInfo.isLogin)
+    }
+
     return {
-      list
+      list,
+      userInfo,
+      userLoign
     }
   },
   components: {
-    ColumnList
+    ColumnList,
+    GlobalHeader
   }
 })
 </script>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
+    <!-- <div class="container-fluid"> -->
+    <a href="#" class="navbar-brand">者也专栏</a>
+    <!-- </div> -->
+    <ul v-if="!user.isLogin" class="list-inline mb-0">
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light my-2" @click="userLoginBtn">登录</a>
+      </li>
+      <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">注册</a></li>
+    </ul>
+    <ul v-else class="list-inline mb-0">
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light my-2">你好 {{ user.name }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export interface UserProps {
+  isLogin: boolean
+  name?: string
+  id?: number
+}
+export default defineComponent({
+  name: 'GlobalHeader',
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  },
+  emits: {
+    userLogin: null
+  },
+  setup(props, context) {
+    const userLoginBtn = () => {
+      context.emit('userLogin')
+    }
+    return {
+      userLoginBtn
+    }
+  }
+})
+</script>
